d69a5dc1406d52a1a671749faf777a85e50bf2a5
60° <hatsumi.yamane.4609@gmail.com>
2019-12-09 18:12:18 +0900

    edit eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,15 @@ module.exports = {
   extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+        tabWidth: 2
+      }
+    ]
   },
   parserOptions: {
     parser: "@typescript-eslint/parser"
