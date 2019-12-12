6dce4a6b2bdf21c661d436cad0601e4b2473f07e
60° <hatsumi.yamane.4609@gmail.com>
2019-12-13 08:56:48 +0900

    テンプレート構文

--- src/components/MyHelloWorld.vue
+++ src/components/MyHelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <!-- この中にHTMLを記述 -->
+    <p>{{ val }}</p>
+    <div>{{ val === 'foo' ? 'A' : 'B' }}</div>
   </div>
 </template>
 
@@ -10,7 +12,9 @@ import { Vue, Component, Prop, Emit, Watch } from 'vue-property-decorator'
 @Component({
   components: {}
 })
-export default class extends Vue {}
+export default class MyHelloWorld extends Vue {
+  val: string = 'foo'
+}
 </script>
 
 <style lang="scss" scoped></style>
