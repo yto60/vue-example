fbbbd9c3bdad7c2158353e0a6b4737a9d80be42a
60° <hatsumi.yamane.4609@gmail.com>
2019-12-13 08:44:53 +0900

    Template.vueを修正

--- src/components/Template.vue
+++ src/components/Template.vue
@@ -8,9 +8,14 @@
 import { Vue, Component, Prop, Emit, Watch } from 'vue-property-decorator'
 
 @Component({
-  components: {}
+  components: {
+    // この中に子コンポーネント一覧を記述
+  }
 })
-export default class extends Vue {}
+export default class extends Vue {
+  // 上の行は export default class <コンポーネント名> extends Vue に書き換えてください
+  // この中に変数、関数などを記述
+}
 </script>
 
 <style lang="scss" scoped></style>
