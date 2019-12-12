e846286cc4cf85487fc42d0609602bc1981b6e02
60° <hatsumi.yamane.4609@gmail.com>
2019-12-13 08:23:55 +0900

    add Template.vue

---
+++ src/components/Template.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- この中にHTMLを記述 -->
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Emit, Watch } from 'vue-property-decorator'
+
+@Component({
+  components: {}
+})
+export default class extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
